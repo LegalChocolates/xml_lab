e7a515f1358dc5d816e58df813870668f55dad86
Jon Deluz <jondeluz@hotmail.com>
2016-03-21 15:06:20 -0700

    Added bingo search functionality

---
+++ application/views/search.php
@@ -0,0 +1,11 @@
+<h1>{message}</h1>
+<h3>{course_message}</h3>
+<h3>{day_message}</h3>
+<h3>{period_message}</h3>
+<hr>
+<h5>Timeslot: {timeslot}</h5>
+<h5>Day: {day}</h5>
+<h5>Instructor: {instructor}</h5>
+<h5>Room: {room}</h5>
+<h5>Booking Type: {bookingtype}</h5>
+<h5>Course Name: {coursename}</h5>
